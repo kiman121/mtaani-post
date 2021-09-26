e4411dca8ffada577311c18a025fffacdc69b028
Patrick <kimanthip@gmail.com>
2021-09-26 10:59:46 +0300

    Add logout, profile and users paths

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('users/', views.users, name='users')
 ]
