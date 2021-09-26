7cd1974d51f2c83faea8db330e740f21b6dc827d
Patrick <kimanthip@gmail.com>
2021-09-26 10:54:50 +0300

    Add home, businees and neighbourhood list view fundtionc

--- neighbourhoods/views.py
+++ neighbourhoods/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'neighbourhoods/home.html')
+
+def businessList(request):
+    return render(request, 'neighbourhoods/business-list.html')
+
+def neighbourhoodList(request):
+    return render(request, 'neighbourhoods/neighbourhood-list.html')
