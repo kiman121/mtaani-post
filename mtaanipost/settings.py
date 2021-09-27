1931b8b4c4459e665ef9d6d321e09d65b22aa246
Patrick <kimanthip@gmail.com>
2021-09-27 04:46:46 +0300

    Add email configurations

--- mtaanipost/settings.py
+++ mtaanipost/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'users.apps.UsersConfig',
     'neighbourhoods.apps.NeighbourhoodsConfig',
+
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,6 +121,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Email configurations 
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
