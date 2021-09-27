7938003ea00c10dadac31565970505af74052cf8
Patrick <kimanthip@gmail.com>
2021-09-27 09:26:10 +0300

    Add paths for add contact and add post

--- neighbourhoods/urls.py
+++ neighbourhoods/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('business-list/', views.businessList, name='business-list'),
     path('neighbourhood-list/',views.neighbourhoodList, name='neighbourhood-list'),
     path('add-neighbourhood/', views.addNeighbourhood, name='add-neighbourhood'),
+    path('add-contact/', views.addContact, name='add-contact'),
+    path('add-post/', views.addPost, name='add-post'),
 ]
