c34b8aedeb9fa3d85aae09b14b5e64d1524fd461
Patrick <kimanthip@gmail.com>
2021-09-27 09:24:05 +0300

    Register Post model onto admin site

--- neighbourhoods/admin.py
+++ neighbourhoods/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Neighbourhood
+from .models import Neighbourhood, Post
 # Register your models here.
 admin.site.register(Neighbourhood)
+admin.site.register(Post)
