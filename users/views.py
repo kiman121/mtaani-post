2abb014ead9637b06979d6bbbc60c9548d283fd4
Patrick <kimanthip@gmail.com>
2021-09-27 04:53:30 +0300

    Add register user and edit profile view functions

--- users/views.py
+++ users/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-
+from .forms import CustomUserCreationForm, ProfileForm
+from .models import Profile
 # Create your views here.
 def loginUser(request):
 
@@ -30,6 +31,41 @@ def loginUser(request):
 
     return render(request, 'users/login.html')
 
+def registerUser(request):
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(
+                request, 'An error has occurred during registration!')
+
+
+    return render(request, 'users/register.html', {'form':form})
+
+@login_required(login_url='login')
+def editProfile(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    return render(request, 'users/profile.html', {'form':form})
+
+
 def logoutUser(request):
     logout(request)
     messages.info(request, 'User was logged out!')
@@ -37,8 +73,13 @@ def logoutUser(request):
     
 @login_required(login_url='login')
 def profile(request):
-    return render(request, 'users/profile.html')
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    return render(request, 'users/profile.html', {'form':form})
 
 @login_required(login_url='login')
 def users(request):
-    return render(request, 'users/users.html')
+    form = ProfileForm()
+    profiles = Profile.objects.all()
+    return render(request, 'users/users.html', {'profiles':profiles, 'form':form})
