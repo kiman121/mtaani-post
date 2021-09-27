adc52faa687f08acdea8ccd63f34c7d9d62a0e80
Patrick <kimanthip@gmail.com>
2021-09-27 04:52:19 +0300

    Add paths for register and edit user

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('register-user/', views.registerUser, name='register-user'),
+    path('edit-user/', views.editProfile, name='edit-user'),
     path('profile/', views.profile, name='profile'),
     path('users/', views.users, name='users')
 ]
