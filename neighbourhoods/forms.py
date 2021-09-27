6fb8bf0975c6daaee1d9d241e0cecf43a4585d1a
Patrick <kimanthip@gmail.com>
2021-09-27 09:24:45 +0300

    Add Contact and Post forms

--- neighbourhoods/forms.py
+++ neighbourhoods/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Neighbourhood
+from .models import Neighbourhood, Contact, Post
 
 class NeighbourhoodForm(ModelForm):
 
@@ -10,6 +10,34 @@ class NeighbourhoodForm(ModelForm):
         
     def __init__(self, *args, **kwargs):
         super(NeighbourhoodForm, self).__init__(*args, **kwargs)
+        
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
+
+class ContactForm(ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = {'name','email','phone_number','location', 'category'}
+        
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
+
+class PostForm(ModelForm):
+
+    class Meta:
+        model = Post
+        fields = {'post_type','title','description'}
+        
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
         
         
         for name, field in self.fields.items():
