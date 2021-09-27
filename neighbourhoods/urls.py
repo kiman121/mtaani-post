417ad355fa998494d724f97cae43d438f772485e
Patrick <kimanthip@gmail.com>
2021-09-27 04:49:16 +0300

    Add path for add-neighbourhood

--- neighbourhoods/urls.py
+++ neighbourhoods/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('business-list/', views.businessList, name='business-list'),
-    path('Neighbourhood-list',views.neighbourhoodList, name='neighbourhood-list'),
+    path('neighbourhood-list/',views.neighbourhoodList, name='neighbourhood-list'),
+    path('add-neighbourhood/', views.addNeighbourhood, name='add-neighbourhood'),
 ]
