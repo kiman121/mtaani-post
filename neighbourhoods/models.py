ba47f67ec152a8c56858f321f8bf53d903140aa1
Patrick <kimanthip@gmail.com>
2021-09-27 09:25:28 +0300

    Add contact and post models

--- neighbourhoods/models.py
+++ neighbourhoods/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+
+
 # Create your models here.
 class Neighbourhood(models.Model):
     owner = models.ForeignKey(
@@ -14,3 +16,41 @@ class Neighbourhood(models.Model):
 
     def __str__(self):
         return str(self.name)
+    
+class Post(models.Model):
+    POST_TYPES = (
+        ('news', 'News'),
+        ('announcement', 'Announcement')
+    )
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    neighbourhood = models.ForeignKey(
+        Neighbourhood, on_delete=models.CASCADE, null=True)
+    post_type =models.CharField(max_length=200, choices=POST_TYPES)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.title)
+
+class Contact(models.Model):
+    CATEGORY = (
+        ('health', 'Health'),
+        ('security', 'Security')
+    )
+    neighbourhood = models.ForeignKey(
+        Neighbourhood, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    phone_number = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    category =models.CharField(max_length=200, choices=CATEGORY)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
