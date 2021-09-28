33bb25ede2fca33978aae278fc26d47ab9d101b7
Patrick <kimanthip@gmail.com>
2021-09-28 06:12:07 +0300

    Add business model

--- neighbourhoods/models.py
+++ neighbourhoods/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+# from users.models import Profile
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -51,6 +52,17 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=200, blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
     category =models.CharField(max_length=200, choices=CATEGORY)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+class Business(models.Model):
+    neighbourhood = models.ForeignKey(
+        Neighbourhood, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
