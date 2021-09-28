9633f5d840015418b2d66890eb2fdeb6268c0b66
Patrick <kimanthip@gmail.com>
2021-09-28 06:10:13 +0300

    Register the busniess model to the admin site

--- neighbourhoods/admin.py
+++ neighbourhoods/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Neighbourhood, Post
+from .models import Neighbourhood, Post, Business
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Post)
+admin.site.register(Business)
